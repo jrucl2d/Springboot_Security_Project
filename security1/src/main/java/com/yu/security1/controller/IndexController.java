1673faf00e7e6117d3c59cde6a84d44f1eed3c86
jrucl2d <ysk789465@gmail.com>
2020-11-16 20:26:23 +0900

    Spring security를 사용한 회원가입 완료

--- security1/src/main/java/com/yu/security1/controller/IndexController.java
+++ security1/src/main/java/com/yu/security1/controller/IndexController.java
@@ -1,11 +1,23 @@
 package com.yu.security1.controller;
 
+import com.yu.security1.model.User;
+import com.yu.security1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder encodePwd;
+
     @GetMapping({"", "/"})
     public String index(){
         // mustache 기본 폴더 : src/main/resources/
@@ -27,16 +39,22 @@ public class IndexController {
     }
 
     // Spring Security가 이 주소를 낚아 챔 - Security Config 파일에서 설정해서 비활성화 됨
-    @GetMapping("/login")
-    public @ResponseBody String login(){
-        return "login";
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "loginForm";
     }
-    @GetMapping("/join")
-    public @ResponseBody String join(){
-        return "join";
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "joinForm";
     }
-    @GetMapping("/joinProc")
-    public @ResponseBody String joinProc(){
-        return "회원가입 완료됨!";
+    @PostMapping("/join")
+    public String join(User user){
+        user.setRole("ROLE_USER");
+
+        String rawPw = user.getPassword();
+        String encPw = encodePwd.encode(rawPw);
+        user.setPassword(encPw);
+        userRepository.save(user);
+        return "redirect:/loginForm";
     }
 }
---
+++ security1/src/main/java/com/yu/security1/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.yu.security1.repository;
+
+import com.yu.security1.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// CRUD 함수를 들고 있다.
+// @Repository는 없어도 된다. JpaRepository를 상속했기 때문에.
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
