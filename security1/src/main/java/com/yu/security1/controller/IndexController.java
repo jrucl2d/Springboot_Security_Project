23015e1a7775fffcc41c06be1106238967da1523
jrucl2d <ysk789465@gmail.com>
2021-01-23 20:58:44 +0900

    기본 인덱스 페이지확인

--- security1/src/main/java/com/yu/security1/controller/IndexController.java
+++ security1/src/main/java/com/yu/security1/controller/IndexController.java
@@ -3,14 +3,11 @@ package com.yu.security1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller // view를 리턴하겠다
+@Controller
 public class IndexController {
 
-    @GetMapping({ "", "/" })
+    @GetMapping({"", "/"})
     public String index() {
-        // mustache 기본 폴더 : src/main/resources/
-        // ViewResolver 설정 : templates(prefix), .mustache(suffix)
-        // 위 설정이 기본으로 잡히므로 생략 가능
         return "index";
     }
 }
